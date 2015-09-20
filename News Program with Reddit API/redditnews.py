a750b6fce2ee5b4a268c7540349450bf43cd7b43
zak_doesgaming <zakdoesgaming@gmail.com>
2015-09-20 11:02:11 +0100

    Added URL of post to output.

--- News Program with Reddit API/redditnews.py
+++ News Program with Reddit API/redditnews.py
@@ -2,14 +2,13 @@ import praw
 
 agent = praw.Reddit(user_agent = 'Reddit_News_Application')
 articleLimit = int(input("Limit of articles (More articles = More time): "))
-printToFile = input("Print output to file? ").lower()
 articles = agent.get_subreddit('news').get_hot(limit = articleLimit)
 f = open('news.txt', 'w')
 
 print("\n" + "Articles:" + "\n")
 
 for x in articles:
-    print(str(x) + "\n")
+    print(str(x) + "\n" + x.url + "\n")
     f.write(str(x) + "\n")
 
 f.close()
